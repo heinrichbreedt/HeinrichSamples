23d3cb24e4940beab12e93f990ae30ce2e153238
hbreedt <hbreedt@calumo.com>
2011-04-10 21:07:10 +1000

    first failing test

---
+++ chequewriter/src/chequetools.test/NumberToWordsFixture.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using Shouldly;
+
+namespace chequetools.test
+{
+    [TestFixture]
+    public class NumberToWordsFixture
+    {
+        [TestCase("1", "One")]
+        public void SingleDigit(string number, string word)
+        {
+            number.ToWord().ShouldBe(word);
+        }
+    }
+}
---
+++ chequewriter/src/chequetools/NumberToWord.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace chequetools
+{
+    public static class NumberToWord
+    {
+        public static string ToWord(this string number)
+        {
+            return "error";
+        }
+    }
+
+}
